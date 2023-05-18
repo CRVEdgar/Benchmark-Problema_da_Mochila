c79e6a12e062d0f78db0e725865a7b522267b806
Edgar Melo <edgarmelo@acad.ifma.edu.br>
2023-05-17 22:18:28 -0300

    inclusão de logs e remoção de verificações desnecessárias

--- src/main/java/org/service/BranchBoundService.java
+++ src/main/java/org/service/BranchBoundService.java
@@ -13,28 +13,21 @@ public class BranchBoundService {
 //   public static Double capacidadeMochila = 524.0;
 
     public Double branchAndBound(List<Objeto> objetos) {
-//        Arrays.sort(objetos, (i1, i2) -> {
-//            double ratio1 = i1.valor / (double) i1.peso;
-//            double ratio2 = i2.valor / (double) i2.peso;
-//            if (ratio1 > ratio2) {
-//                return -1;
-//            } else if (ratio1 < ratio2) {
-//                return 1;
-//            }
-//            return 0;
-//        });
+
 
         Nodo melhorNodo = new Nodo(-1, 0.0, 0.0, 0.0); //Instancia o primeiro nodo
-        Nodo nodoAtual = new Nodo(-1, 0.0, 0.0, calculaValorEstimado(objetos, melhorNodo)); //Instancia o nodo atual com
+        Nodo nodoAtual = new Nodo(-1, 0.0, 0.0, calculaValorEstimado(objetos, melhorNodo)); //Instancia o nodo atual com dados do primeiro, iniciando o valor Estimado
 
         int i=0;
-        while (nodoAtual.getNivel() < objetos.size() /*- 1*/) {
+        //PERCORRE TODA LISTA
+        while (nodoAtual.getNivel() < objetos.size()) {
 //            i++;
 //            System.out.println("CICLO: " + (objetos.size()));
             nodoAtual.addNivel();
 
-            Double pesoAtual = nodoAtual.getPesoTotal() + objetos.get(nodoAtual.getNivel()).getPeso();
+            Double pesoAtual = nodoAtual.getPesoTotal() + objetos.get(nodoAtual.getNivel()).getPeso(); //ATUAL + PROXIMO DA LISTA
             Double valorAtual = nodoAtual.getValorTotal() + objetos.get(nodoAtual.getNivel()).getValorTotal();
+            System.out.println("PESO ATUAL: " + pesoAtual + " | VALOR ATUAL: " + valorAtual);
 
             if (pesoAtual <= LIMITE_MOCHILA && valorAtual > melhorNodo.getValorTotal()) {
                 melhorNodo.setValorTotal(valorAtual);
@@ -44,7 +37,7 @@ public class BranchBoundService {
 
             Double valorEstimado = calculaValorEstimado(objetos, nodoAtual);
 
-            if (valorEstimado > melhorNodo.getValorTotal() && pesoAtual < 524) {
+            if (valorEstimado > melhorNodo.getValorTotal()) {
                 Nodo ramoEsquerdo = new Nodo(nodoAtual.getNivel(), valorAtual, pesoAtual, valorEstimado);
                 melhorNodo = branchAndBoundRecursivo(objetos, ramoEsquerdo, melhorNodo);
                 System.out.println("MELHOR NODO: " + melhorNodo);
@@ -55,14 +48,15 @@ public class BranchBoundService {
 
             nodoAtual.decrementValorTotal( objetos.get(nodoAtual.getNivel()).getValorTotal() );
 
-            if (nodoAtual.getPesoTotal() <= LIMITE_MOCHILA && nodoAtual.getValorTotal() + valorEstimado > melhorNodo.getValorTotal() && nodoAtual.getPesoTotal() < 524) {
+            if (nodoAtual.getPesoTotal() <= LIMITE_MOCHILA && nodoAtual.getValorTotal() + valorEstimado > melhorNodo.getValorTotal()) {
                 Nodo ramoDireito = new Nodo(nodoAtual.getNivel(), nodoAtual.getValorTotal(), nodoAtual.getPesoTotal(), valorEstimado);
                 melhorNodo = branchAndBoundRecursivo(objetos, ramoDireito, melhorNodo);
                 System.out.println("MELHOR NODO: " + melhorNodo);
             }
 
-            if (nodoAtual.getPesoTotal() <= LIMITE_MOCHILA && nodoAtual.getValorTotal() + valorEstimado > melhorNodo.getValorTotal() && nodoAtual.getPesoTotal() < 524) {
+            if (nodoAtual.getPesoTotal() <= LIMITE_MOCHILA && nodoAtual.getValorTotal() + valorEstimado > melhorNodo.getValorTotal()) {
                 Nodo ramoSuperior = new Nodo(nodoAtual.getNivel(), nodoAtual.getValorTotal(), nodoAtual.getPesoTotal(), valorEstimado);
+                //TODO: INCLUIR NA LISTA DE ITENS NA MOCHILA O OBJETO DESTE NODO
                 nodoAtual = ramoSuperior;
             } else {
                 System.out.println("LIMITE ATINGIDO NO NIVEL " + nodoAtual.getNivel());
@@ -79,30 +73,44 @@ public class BranchBoundService {
         Double valorEstimado = nodo.getValorTotal();
 
         int nivel = nodo.getNivel() + 1;
+        System.out.println("iniciará o nivel: " + nivel);
 
         while (nivel < objetos.size() && pesoRestante >= objetos.get(nivel).getPeso()) {
-            pesoRestante -= objetos.get(nivel).getPeso();
-            valorEstimado += objetos.get(nivel).getValorTotal();
+            System.out.println(pesoRestante >= objetos.get(nivel).getPeso());
+            System.out.println("nivel: " + nivel + " to object: " + objetos.get(nivel).getRowIndex() );
+            pesoRestante -= objetos.get(nivel).getPeso(); //limite reduz pois: ADD O PESO DO OBJETO NA MOCHILA
+            valorEstimado += objetos.get(nivel).getValorTotal(); //acrescenta o valor total de cada objeto
+
+            System.out.println("Peso Restante: " + pesoRestante  +  " Valor Estimado: " + valorEstimado
+                    + "\n Peso do objeto: " + objetos.get(nivel).getPeso() + " | Valor Total: " + objetos.get(nivel).getValorTotal());
+            System.out.println("\n");
             nivel++;
         }
+        System.out.println("SAIU DO LAÇO");
 
         if (nivel < objetos.size()) {
             valorEstimado += (pesoRestante / objetos.get(nivel).getPeso()) * objetos.get(nivel).getValorTotal();
         }
+        System.out.println("VALOR ESTIMADO APÓS DIVISAO: " + valorEstimado);
 
         return valorEstimado;
     }
 
     private Nodo branchAndBoundRecursivo(List<Objeto> objetos, Nodo nodoAtual, Nodo melhorNodo) {
-        if (nodoAtual.getPesoTotal() > LIMITE_MOCHILA || nodoAtual.getPesoTotal() >= 524) {
+        if (nodoAtual.getPesoTotal() > LIMITE_MOCHILA) {
+            System.out.println(" * NODO INCLUIDO: " + melhorNodo.getPesoTotal() + " || " + melhorNodo.getValorTotal());
             //todo: esse retorno deve ser adicionado na lista
             return melhorNodo;
         }
 
-        if (nodoAtual.getNivel() == objetos.size() - 1) { //TODO: size only / no -1
+        if (nodoAtual.getNivel() == objetos.size() - 1) {
             if (nodoAtual.getValorTotal() > melhorNodo.getValorTotal()) {
+                //todo: esse retorno deve ser adicionado na lista
+                System.out.println(" ** NODO INCLUIDO: " + nodoAtual.getPesoTotal() + " || " + nodoAtual.getValorTotal());
                 return nodoAtual;
             } else {
+                //todo: esse retorno deve ser adicionado na lista
+                System.out.println(" *** NODO INCLUIDO: " + melhorNodo.getPesoTotal() + " || " + melhorNodo.getValorTotal());
                 return melhorNodo;
             }
         }
@@ -116,6 +124,8 @@ public class BranchBoundService {
         Nodo ramoDireito = new Nodo(proximoNivel, nodoAtual.getValorTotal(), nodoAtual.getPesoTotal(), nodoAtual.getValorEstimado());
         melhorNodo = branchAndBoundRecursivo(objetos, ramoDireito, melhorNodo);
 
+        //todo: esse retorno deve ser adicionado na lista
+        System.out.println(" **** NODO INCLUIDO: " + melhorNodo.getPesoTotal() + " || " + melhorNodo.getValorTotal());
         return melhorNodo;
     }
 }
--- src/main/java/org/util/DirFile.java
+++ src/main/java/org/util/DirFile.java
@@ -3,5 +3,5 @@ package org.util;
 public class DirFile {
 
     public static final Double LIMITE_MOCHILA = 524.0;
-    public static final String DIR = "/media/edgar/ARMAZENAMENTO COMPARTILHADO/FACULDADE/MESTRADO/Algoritmo de Otimização/branch_bound/benchmark/src/main/java/org/util/mochila.xlsx";
+    public static final String DIR = "/home/edgar/Documentos/FACULDADE/MESTRADO/benchmark/src/main/java/org/util/mochila.xlsx";
 }
